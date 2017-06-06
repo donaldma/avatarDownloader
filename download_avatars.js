482827e2d6c87722b1bdd7ebd21afd52169d5f3a
donaldma <donald.ma@hotmail.ca>
2017-06-06 02:43:03 +0000

    Added Step 6

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const GITHUB_USER = 'donaldma';
 const GITHUB_TOKEN = 'eefbcc7bfca09f9c108e2e53463912e95f8d0c78';
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + "jquery" + '/' + "jquery" + '/contributors';
-var emptyArr = [];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -19,10 +18,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 
 getRepoContributors("jquery", "jquery", function (err, result){
+  var emptyArr = [];
   var img = JSON.parse(result.body);
   img.forEach(function(item, index, array) {
-  //   emptyArr.push(item.avatar_url);
-    console.log(item.avatar_url)
-  })
+    emptyArr.push(item.avatar_url);
+  });
+  console.log(emptyArr);
 });
-
