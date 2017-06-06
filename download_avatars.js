cb80fd514e5d0d206f67858ca88959ca419ef7cc
donaldma <donald.ma@hotmail.ca>
2017-06-06 06:15:15 +0000

    Added final product

--- download_avatars.js
+++ download_avatars.js
@@ -1,27 +1,31 @@
-const request = require('request');
-const fs = require('fs');
-const GITHUB_USER = 'donaldma';
-const GITHUB_TOKEN = 'eefbcc7bfca09f9c108e2e53463912e95f8d0c78';
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + "jquery" + '/' + "jquery" + '/contributors';
-
-console.log('Welcome to the GitHub Avatar Downloader!');
+var request = require('request');
+var fs = require('fs');
+var GITHUB_USER = 'donaldma';
+var GITHUB_TOKEN = 'eefbcc7bfca09f9c108e2e53463912e95f8d0c78';
+var owner = process.argv[2];
+var repo = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var options = {
     url: requestURL,
     headers: {
-      'User-Agent' : 'donaldma'
-    }
+      'User-Agent': 'donaldma'
+    },
+    json: true
   };
-  request.get(options, cb)
-};
-
-
-getRepoContributors("jquery", "jquery", function (err, result){
-  var emptyArr = [];
-  var img = JSON.parse(result.body);
-  img.forEach(function(item, index, array) {
-    emptyArr.push(item.avatar_url);
+  request.get(options, function(err, response, body){
+    cb(err, body);
   });
-  console.log(emptyArr);
+}
+
+function downloadImageByURL(url, filePath) {
+  request(url)
+  .pipe(fs.createWriteStream(filePath));
+}
+
+getRepoContributors(owner, repo, function(err, body) {
+  body.forEach(function(item, index, array){
+    downloadImageByURL(item.avatar_url, './images/' + item.login + '.jpg');
+  });
 });
